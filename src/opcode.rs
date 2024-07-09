cdeddbd07ed5a0a1437b0cb73f08e55abbf90494
techmetx11 <techmetx11@disroot.org>
2024-07-09 22:06:27 +0100

    Switch the byteswapping to regular shifting

--- src/opcode.rs
+++ src/opcode.rs
@@ -60,7 +60,7 @@ impl Decoder for OpcodeDecoder {
                     return Ok(None);
                 }
 
-                let sig_size: u16 = u16::from_be_bytes(src[5..6].try_into().unwrap());
+                let sig_size: u16 = ((src[5] as u16) << 8) | src[6] as u16;
 
                 if (usize::from(sig_size) + 7) > src.len() {
                     return Ok(None);
