2d19eef75d9f44242b178e9a8c22d759e6c38236
Adam Bambuch <bambuch@vshosting.cz>
2025-02-12 20:38:24 +0100

    yt-dlp nsig and sig decoders scripts

---
+++ src/scripts/yt-dlp_nsig_decoder.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import yt_dlp.YoutubeDL
+import yt_dlp.extractor.youtube
+import sys
+import os
+import json
+
+"""
+Args:
+    - sys.argv[1]: player_url
+    - sys.argv[2]: signature
+Example:
+    python yt-dlp_nsig_decoder.py "https://www.youtube.com/s/player/af7f576f/player_ias.vflset/en_US/base.js" "W78n255zM6g"
+"""
+
+params = {}
+
+YOUTUBE_VIDEO_ID = "W78n255zM6g"
+
+ie = yt_dlp.extractor.YoutubeIE()
+ydl = yt_dlp.YoutubeDL({})
+ydl.add_info_extractor(ie)
+
+player_url = sys.argv[1]
+signature = sys.argv[2]
+print(type(signature))
+
+print(ie._decrypt_nsig(signature, YOUTUBE_VIDEO_ID, player_url))
